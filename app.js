7d741395c5d162d7d8abddb4bc9cd4b7409d9bc0
hariyono1995 <onnelikrio@gmail.com>
2022-04-13 22:50:16 +0700

    typescryp basic

---
+++ app.js
@@ -0,0 +1,9 @@
+// function add(numb1: number, numb2: number) {
+//   return numb1 + numb2;
+// }
+// console.log(add(2, 2));
+//  make object
+var person = {
+    name: "John",
+    age: 30
+};
---
+++ app.ts
@@ -0,0 +1,16 @@
+// function add(numb1: number, numb2: number) {
+//   return numb1 + numb2;
+// }
+
+// console.log(add(2, 2));
+
+//  make object
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: "John",
+  age: 30,
+};
+
+console.log(person);
