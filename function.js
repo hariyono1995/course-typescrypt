62daa74ce5d4c7a337449b682bcf2c9e2707de50
hariyono1995 <onnelikrio@gmail.com>
2022-04-14 12:57:30 +0700

    function typescript

--- function.js
+++ function.js
@@ -1,3 +1,25 @@
 "use strict";
 exports.__esModule = true;
-console.log("print function typescript");
+//  function dengan tipe data yang di return
+//  string
+function getName() {
+    return "Jhon Dow";
+    // return 888; //error harus string
+}
+console.log(getName());
+//  number
+function getNumber() {
+    return 888;
+}
+console.log(getNumber());
+//  function yang tidak mereturn apapun dan tidak boleh ada return di dalam function
+function printName() {
+    console.log(getName());
+}
+printName();
+//  argument type di dalam function
+function multiple(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiple(2, 3));
+//  function as type / func sebagai type data
--- function.ts
+++ function.ts
@@ -1,3 +1,36 @@
 export {};
 
-console.log("print function typescript");
+//  function dengan tipe data yang di return
+
+//  string
+function getName(): string {
+  return "Jhon Dow";
+  // return 888; //error harus string
+}
+
+console.log(getName());
+
+//  number
+function getNumber(): number {
+  return 888;
+}
+
+console.log(getNumber());
+
+//  function yang tidak mereturn apapun dan tidak boleh ada return di dalam function
+
+function printName(): void {
+  console.log(getName());
+}
+
+printName();
+
+//  argument type di dalam function
+
+function multiple(num1: number, num2: number): number {
+  return num1 * num2;
+}
+
+console.log(multiple(2, 3));
+
+//  function as type / func sebagai type data
