c4be0b9617fe59e3acbb7eb0d69ff07b6cf56385
hariyono1995 <onnelikrio@gmail.com>
2022-04-14 12:07:03 +0700

    type data

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+"use strict";
+exports.__esModule = true;
+// type data
+//  string
+var name = "Max";
+//  number
+var umur = 27;
+//  boolean
+var isMarried = false;
+//  any -> memungkinkan bisa mengisi variabel dengan berbagai tipe data
+var password = "ikehieek";
+password = 27;
+password = true;
+password = ["hello"];
+password = { password: "password" };
+//  array
+var hobbies = ["read book", "write code"];
+// hobbies =[8] // error
+var randomNum = [1, 2, 3, 4, 5, 6, 7];
+// randomNum['string'] //error
+var address = ["jln. abc no.", 8, true];
+// tuples -> array yang mana tipe datanya sudah ditentukan di dalamnya dan jumlah itemnya pun terbatas
+var biodata = ["John", 27];
+// biodata= ["John", 27, "John"];  // error melebihi batas jumlah item yang telah di tentukan
--- index.ts
+++ index.ts
@@ -1,1 +1,33 @@
-console.log("Hello World");
+export {};
+
+// type data
+
+//  string
+let name: string = "Max";
+
+//  number
+let umur: number = 27;
+
+//  boolean
+let isMarried: boolean = false;
+
+//  any -> memungkinkan bisa mengisi variabel dengan berbagai tipe data
+let password: any = "ikehieek";
+password = 27;
+password = true;
+password = ["hello"];
+password = { password: "password" };
+
+//  array
+let hobbies: string[] = ["read book", "write code"];
+// hobbies =[8] // error
+
+let randomNum: number[] = [1, 2, 3, 4, 5, 6, 7];
+// randomNum['string'] //error
+
+let address: any[] = ["jln. abc no.", 8, true];
+
+// tuples -> array yang mana tipe datanya sudah ditentukan di dalamnya dan jumlah itemnya pun terbatas
+
+let biodata: [string, number] = ["John", 27];
+// biodata= ["John", 27, "John"];  // error melebihi batas jumlah item yang telah di tentukan
