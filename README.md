b0f33485f33f45d78ae46549664892e136ee0851
hariyono1995 <onnelikrio@gmail.com>
2022-04-14 11:20:50 +0700

    type data typescrypt

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # course-typescrypt
+
+1. npm init
+2. install npm install typescript --save-dev in your project
+3. ./node_modules/.bin/tsc --init
--- app.js
+++
@@ -1,9 +0,0 @@
-// function add(numb1: number, numb2: number) {
-//   return numb1 + numb2;
-// }
-// console.log(add(2, 2));
-//  make object
-var person = {
-    name: "John",
-    age: 30
-};
