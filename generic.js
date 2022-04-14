abc14e101cb5e0c24121d205c17800428e9348bc
hariyono1995 <onnelikrio@gmail.com>
2022-04-14 23:14:02 +0700

    generic basic

--- generic.js
+++ generic.js
@@ -1,1 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 console.log("generic");
+function getData(value) {
+    return value;
+}
+console.log(getData("Jhon").length); // 4
+console.log(getData(123).length); // undefined (untuk mengatasi ini bisa menggunakan generic)
+function myData(value) {
+    return value;
+}
+console.log(myData("Jhon").length); //4
+// console.log(myData(123).length); // error karena number tidak bisa  di gunakan untuk type number
+//  penulisan untuk arrow function
+var Data = function (value) {
+    return value;
+};
--- generic.ts
+++ generic.ts
@@ -1,1 +1,21 @@
+export {};
 console.log("generic");
+
+function getData(value: any) {
+  return value;
+}
+
+console.log(getData("Jhon").length); // 4
+console.log(getData(123).length); // undefined (untuk mengatasi ini bisa menggunakan generic)
+
+function myData<T>(value: T) {
+  return value;
+}
+
+console.log(myData("Jhon").length); //4
+// console.log(myData(123).length); // error karena number tidak bisa  di gunakan untuk type number
+
+//  penulisan untuk arrow function
+const Data = <T>(value: T) => {
+  return value;
+};
