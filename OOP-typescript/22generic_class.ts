8016c23deb3aa6b4fe68ecbd336799bf856b59ec
hariyono1995 <onnelikrio@gmail.com>
2022-04-17 08:52:19 +0700

    generic class

--- OOP-typescript/22generic_class.ts
+++ OOP-typescript/22generic_class.ts
@@ -17,3 +17,26 @@ class GenericClass<T> implements Generic2<T> {
 
 const generic = new GenericClass<string>("Hello Dunia");
 console.log(generic.methodA());
+
+//  memasukkan class menjadi generic type data
+
+class ClassA {
+  public _total: number;
+  constructor(total: number) {
+    this._total = total;
+  }
+}
+
+class ClassBaru<T> {
+  public _totalStudent: T;
+
+  constructor(totalStudent: T) {
+    this._totalStudent = totalStudent;
+  }
+}
+
+const classA = new ClassA(40);
+
+const classBaru = new ClassBaru(classA);
+
+console.log(classBaru);
