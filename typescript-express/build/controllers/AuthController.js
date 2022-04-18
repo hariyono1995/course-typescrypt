bc930397374835da300403d645f3d80c918dc6c9
hariyono1995 <onnelikrio@gmail.com>
2022-04-18 13:14:53 +0700

    menambahkan encrypt password

--- typescript-express/build/controllers/AuthController.js
+++ typescript-express/build/controllers/AuthController.js
@@ -1,8 +1,33 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const db = require("../db/models");
+const PasswordHash_1 = __importDefault(require("../utils/PasswordHash"));
 class AuthController {
-    register(req, res) {
-        return res.send("register");
+    constructor() {
+        this.register = (req, res) => __awaiter(this, void 0, void 0, function* () {
+            const { username, password } = req.body;
+            const hashedPassword = yield PasswordHash_1.default.passwordHash(password);
+            const createdUser = yield db.user.create({
+                username,
+                password: hashedPassword,
+            });
+            return res.send({
+                msg: "register success",
+                user: { username: createdUser.username },
+            });
+        });
     }
     login(req, res) {
         return res.send("login");
--- typescript-express/src/controllers/AuthController.ts
+++ typescript-express/src/controllers/AuthController.ts
@@ -1,9 +1,25 @@
 import { Request, Response } from "express";
 
+const db = require("../db/models");
+
+import PasswordHash from "../utils/PasswordHash";
+
 class AuthController {
-  public register(req: Request, res: Response): Response {
-    return res.send("register");
-  }
+  public register = async (req: Request, res: Response): Promise<Response> => {
+    const { username, password } = req.body;
+
+    const hashedPassword: string = await PasswordHash.passwordHash(password);
+
+    const createdUser = await db.user.create({
+      username,
+      password: hashedPassword,
+    });
+
+    return res.send({
+      msg: "register success",
+      user: { username: createdUser.username },
+    });
+  };
   public login(req: Request, res: Response): Response {
     return res.send("login");
   }
